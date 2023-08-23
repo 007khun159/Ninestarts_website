2ab74890b874af88e37350d00e2405fd0b4b40da
007khun159 <008khun159@gmail.com>
2023-08-23 16:51:15 +0700

    Ask_page

---
+++ Clone_Design/src/pages/faq.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+
+function faqsection() {
+  return (
+    <div>
+        <section>
+
+            <div className="faq">
+                <div className="titile_header">
+                    <h4>Non consectetur a erat nam at lectus urna duis?</h4>
+                </div>
+                <div className="detail_ask">
+                    <p>Feugiat pretium nibh ipsum consequat. Tempus iaculis urna id volutpat lacus laoreet non curabitur gravida. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus non.</p>
+                </div>
+            </div>
+
+            <div className="faq">
+                <div className="titile_header">
+                    <h4>Feugiat scelerisque varius morbi enim nunc faucibus a pellentesque?</h4>
+                </div>
+                <div className="detail_ask">
+                    <p>Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque elit ullamcorper dignissim. Mauris ultrices eros in cursus turpis massa tincidunt dui.</p>
+                </div>
+            </div>
+            
+            <div className="faq">
+                <div className="titile_header">
+                    <h4>Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi?</h4>
+                </div>
+                <div className="detail_ask">
+                    <p>Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Faucibus pulvinar elementum integer enim. Sem nulla pharetra diam sit amet nisl suscipit. Rutrum tellus pellentesque eu tincidunt. Lectus urna duis convallis convallis tellus. Urna molestie at elementum eu facilisis sed odio morbi quis</p>
+                </div>
+            </div>
+
+            <div className="faq">
+                <div className="titile_header">
+                    <h4>Ac odio tempor orci dapibus. Aliquam eleifend mi in nulla?</h4>
+                </div>
+                <div className="detail_ask">
+                    <p>Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque elit ullamcorper dignissim. Mauris ultrices eros in cursus turpis massa tincidunt dui.</p>
+                </div>
+            </div>
+
+            <div className="faq">
+                <div className="titile_header">
+                    <h4>Tempus quam pellentesque nec nam aliquam sem et tortor consequat?</h4>
+                </div>
+                <div className="detail_ask">
+                    <p>Molestie a iaculis at erat pellentesque adipiscing commodo. Dignissim suspendisse in est ante in. Nunc vel risus commodo viverra maecenas accumsan. Sit amet nisl suscipit adipiscing bibendum est. Purus gravida quis blandit turpis cursus in</p>
+                </div>
+            </div>
+
+            <div className="faq">
+                <div className="titile_header">
+                    <h4>Tortor vitae purus faucibus ornare. Varius vel pharetra vel turpis nunc eget lorem dolor?</h4>
+                </div>
+                <div className="detail_ask">
+                    <p>Laoreet sit amet cursus sit amet dictum sit amet justo. Mauris vitae ultricies leo integer malesuada nunc vel. Tincidunt eget nullam non nisi est sit amet. Turpis nunc eget lorem dolor sed. Ut venenatis tellus in metus vulputate eu scelerisque. Pellentesque diam volutpat commodo sed egestas egestas fringilla phasellus faucibus. Nibh tellus molestie nunc non blandit massa enim nec.</p>
+                </div>
+            </div>
+            
+        </section>
+    </div>
+  )
+}
+
+export default faqsection
+
